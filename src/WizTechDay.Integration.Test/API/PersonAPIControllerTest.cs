cbaba17c18ab0e82a4f4230f00ec9d2a3563006a
Yago Luiz <yago.luiz@hotmail.com>
2019-05-24 19:25:51 -0300

    Finalizacao dos testes de integracao, configuracao de healthcheck e swagger

--- src/WizTechDay.Integration.Test/API/PersonAPIControllerTest.cs
+++ src/WizTechDay.Integration.Test/API/PersonAPIControllerTest.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using WizTechDay.Integration.Test.Mocks;
 using WizTechDay.Web;
 using Xunit;
 
@@ -16,13 +20,41 @@ namespace WizTechDay.Integration.Test.API
         }
 
         [Fact]
-        public async Task ListPerson_HttpStatusOkTest()
+        public async Task ListPerson_SuccessTest()
         {
             var client = _factory.CreateClient();
 
             var response = await client.GetAsync("api/v1/persons");
+            var stringResponse = response.Content.ReadAsStringAsync();
+
+            var personJson = JsonConvert.SerializeObject(stringResponse);
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotEmpty(personJson);
+        }
+
+        [Fact]
+        public async Task CreatePerson_SuccessTest()
+        {
+            var client = _factory.CreateClient();
+
+            var person = PersonMock.PersonViewModelFaker.Generate();
+
+            var response = await client.PostAsync("/api/v1/persons", new StringContent(JsonConvert.SerializeObject(person), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreatePerson_ErroTest()
+        {
+            var client = _factory.CreateClient();
+
+            var person = PersonMock.PersonViewModelFakerError.Generate();
+
+            var response = await client.PostAsync("/api/v1/persons", new StringContent(JsonConvert.SerializeObject(person), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
         }
     }
 }
--- src/WizTechDay.Web/Startup.cs
+++ src/WizTechDay.Web/Startup.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 using WizTechDay.IoC;
 using WizTechDay.Web.Filters;
 using WizTechDay.Web.Services;
 using WizTechDay.Web.Services.Interfaces;
+using WizTechDay.Web.Swagger;
 
+[assembly: ApiConventionType(typeof(MyApiConventions))]
 namespace WizTechDay.Web
 {
     public class Startup
@@ -22,9 +26,27 @@ namespace WizTechDay.Web
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(config => {
+            services.AddMvc(config =>
+            {
                 config.Filters.Add(typeof(CustomExceptionFilter));
             });
+
+            services.AddOpenApiDocument(document =>
+            {
+                document.DocumentName = "v1";
+                document.Version = "v1";
+                document.Title = "Wiz Tech Day API";
+                document.Description = "API de Wiz Tech Day";
+            });
+
+            if (PlatformServices.Default.Application.ApplicationName != "testhost")
+            {
+                services.AddHealthChecksUI()
+                .AddHealthChecks()
+                .AddSqlServer(Configuration.GetConnectionString("wizTechDay"));
+            }
+
+            services.AddHttpContextAccessor();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
@@ -45,20 +67,30 @@ namespace WizTechDay.Web
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
+            if (PlatformServices.Default.Application.ApplicationName != "testhost")
+            {
+                app.UseHealthChecks("/health", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUi3();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-
         }
 
         #region Services
 
         private void RegisterServices(IServiceCollection services)
         {
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IInsertPerson, InsertPerson>();
             services.AddScoped<IListPersonService, ListPersonService>();
 
