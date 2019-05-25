1327f600d26576c3f7adc69e9649f5d644cd5597
Carlos Augusto Ribeiro <carlosaugustoribeiro@wizsolucoes.com.br>
2019-05-25 01:02:42 -0300

    Serviço de edição de deleão e2e

--- e2e/pagemodel/person.js
+++ e2e/pagemodel/person.js
@@ -4,11 +4,15 @@ import Page from './page'
 export default class Person extends Page{
     constructor(){
         super();
-        this.url =  `${this.urlBase}/Person/Create`;
+        this.urlList = `${this.urlBase}/Person/index`;
+        this.urlCreate =  `${this.urlBase}/Person/Create`;
         this.subtitlePage = Selector('[id="subtitle-person"]');
         this.inputName = Selector('[name="Name"]');
         this.inputCpf = Selector('[name="Cpf"]');
         this.inputEmail = Selector('[name="Email"]');
-        this.btnSave = Selector('.btn-success');
+        this.editFirsElementList = Selector('[id="edit-person+1"]');
+        this.deletefirsElementList = Selector('[id="delete-person+1"]');
+        this.btnEdit = Selector('[id="btn-edit"]');
+        this.btnDelete = Selector('[id="btn-delete"]');
     }
 }
--- e2e/person.spec.js
+++ e2e/person.spec.js
@@ -5,12 +5,15 @@ import CpfGenerator from 'gerador-validador-cpf'
 const person = new Person();
 
 fixture('Person')
-    .page(person.url);
+    .page(person.urlList);
 
-test('Deve criar um novo curso', async t => {
+test('Deve criar um novo registro de pessoa', async t => {
 
     await t
-        .typeText(person.inputName, `José ${new Date().toString()}`);
+        .navigateTo(person.urlCreate);
+
+    await t
+        .typeText(person.inputName, "Esmarley");
 
     await t
         .wait(2000);
@@ -22,10 +25,39 @@ test('Deve criar um novo curso', async t => {
         .wait(2000);
 
     await t
-        .typeText(person.inputEmail, 'teste@teste.com')
+        .typeText(person.inputEmail, 'esmarley@wizsolucoes.com.br')
         .click(Selector('.btn-success'))
         .takeScreenshot();
 
     await t
         .expect(person.subtitlePage.innerText).eql('Lista de pessoas')
-})
+});
+
+test('Deve editar o primeiro registro da lista', async t => {
+
+    await t
+        .click(person.editFirsElementList);
+
+    await t
+        .typeText(person.inputName, " Oliveira")
+        .click(person.inputCpf).pressKey('ctrl+a delete')
+        .typeText(person.inputCpf, CpfGenerator.format(CpfGenerator.generate(), 'digits'))
+        .click(person.inputEmail).pressKey('ctrl+a delete')
+        .typeText(person.inputEmail, `esmarleyoliveira@wizsolucoes.com.br`)
+        .click(person.btnEdit);
+
+});
+
+test('Deve deletar o primeiro registro da lista', async t => {
+
+    await t
+        .wait(5000)
+        .click(person.deletefirsElementList);
+
+    await t
+        .click(person.btnDelete);
+
+    await t
+        .wait(5000);
+
+});
